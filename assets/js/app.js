b7c1bea257972d2c0c789bd5b36b25d40330fbcd
MD-Baki <bakimd77@gmail.com>
2022-09-03 13:51:32 +0600

    new add

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,13 +5,9 @@ const loadCategories = () => {
 }
 
 const showCategories = cat => {
-    console.log(cat);
-
     const categoriesContainer = document.getElementById('categories-container');
 
     cat.forEach(category => {
-        console.log(category);
-
         const categoryDiv = document.createElement('li');
         categoryDiv.classList.add('mb-3');
         categoryDiv.innerHTML = `
@@ -20,5 +16,47 @@ const showCategories = cat => {
         categoriesContainer.appendChild(categoryDiv);
     });
 }
-
 loadCategories()
+
+const newsCategory = () => {
+    const url = `https://openapi.programming-hero.com/api/news/category/01`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayNews(data.data))
+}
+
+const displayNews = newsCat => {
+    const newsContainer = document.getElementById('news-container');
+
+    newsCat.forEach(news => {
+        console.log(news);
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('card')
+        newsDiv.innerHTML = `
+            <div class="row align-items-center p-2 rounded">
+                <div class="card_image col-lg-4 pe-2">
+                    <img src="${news.image_url}" class="w-100 rounded-start" alt="...">
+                </div>
+                <div class="card_contante col-lg-8">
+                    <div class="card-body">
+                        <h5 class="card-title fw-bold">${news.title}</h5>
+                        <p class="card-text pt-2 pb-3">${news.details.slice(0, 200)}...</p>
+                        <div
+                            class="card_footer card-text row align-items-center border-top pt-2">
+                            <div class="writer row align-items-center col-6">
+                                <img src="${news.author.img}" class="img-fluid rounded-circle col-5 col-lg-2" alt="...">
+                                <div class="col-7 col-lg-10">
+                                    <p class="fw-bold">${news.author.name}</p>
+                                </div>
+                            </div>
+                            <a href="#" class="fw-bold col-4"><i class="fa-regular fa-eye"></i> ${news.total_view}</a>
+                            <a href="#" class="col-2 text-end"><i class="fa-solid fa-clipboard"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+        newsContainer.appendChild(newsDiv);
+    })
+}
+newsCategory();
